73ac11c58e10c606d53021ca1a061a7edb0a5fbc
Ihor Lushpei <lushpei@Ihors-MacBook-Pro.local>
2022-02-17 21:02:06 +0200

    Updates

---
+++ precache-manifest.447d18f441506fcc6d5a8a9b128fa479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4422a48f95b14f7696a66ac2801ca73",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "290986fe78bef38fe97a",
+    "url": "/monsters-rolodex2/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex2/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex2/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290986fe78bef38fe97a",
+    "url": "/monsters-rolodex2/static/js/main.4d447c73.chunk.js"
+  },
+  {
+    "revision": "5129b2734e446f0ec2c6",
+    "url": "/monsters-rolodex2/static/js/runtime-main.e8342c1e.js"
+  }
+]);
